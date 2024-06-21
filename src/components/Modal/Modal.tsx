0798c9abbde6c3f171c1d0d6bb54f3074eca4bac
nick <nickj8366@gmail.com>
2024-06-21 12:58:48 +0530

    Update Modal.tsx

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -13,7 +13,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, onConfirm }) => {
 
   return (
     <div className="modal-backdrop">
-      <p className='text-white font-semibold text-xl'>Are you sure you want to Disconnect Wallet ?</p>
+      <p className='text-white font-semibold text-xl'>Disconnect Wallet ?</p>
       <Image className='flex justify-center items-center' src="./offline.svg" alt="disconnect" width={250} height={250} />
       <div className='flex justify-center items-center mt-11'>
         <button type="button" onClick={onConfirm} className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-2xl px-11 py-3 text-center me-2 mb-2">Yes</button>
